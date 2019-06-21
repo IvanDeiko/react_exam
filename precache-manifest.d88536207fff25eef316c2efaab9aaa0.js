ec795df8508c9842b6faa1354b156fb396df60f6
Ivan Deiko <ivandeiko1997@gmail.com>
2019-06-21 17:06:56 +0300

    Updates

---
+++ precache-manifest.d88536207fff25eef316c2efaab9aaa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a592822d3d049be7daa5224dd9a666f2",
+    "url": "/react_exam/index.html"
+  },
+  {
+    "revision": "d279d6be96281afd0fa3",
+    "url": "/react_exam/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f85641fd6d859746ade3",
+    "url": "/react_exam/static/js/2.33ac6306.chunk.js"
+  },
+  {
+    "revision": "d279d6be96281afd0fa3",
+    "url": "/react_exam/static/js/main.3899c5d5.chunk.js"
+  },
+  {
+    "revision": "4c3dcd7e4ba5e5cedf14",
+    "url": "/react_exam/static/js/runtime~main.4b58bd7d.js"
+  }
+]);
